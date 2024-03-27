419e4faa189c27526bc81693f22f30f5d4b32be0
tommaho <tom@tommaho.com>
2024-03-27 10:04:06 -0400

    parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
 use std::path::PathBuf;
-use clap::{Parser, Subcommand};
+use clap::Parser; //, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "roll")]
@@ -12,25 +12,51 @@ struct Cli {
     dice: Option<String>,
     modifier_operator: Option<String>,
     modifier_value: Option<String>,
-
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     if let Some(dice) = cli.dice.as_deref() {
-        println!("Value for dice: {dice}");
+
+        let dice = dice.to_lowercase();
+
+        //parse out times to roll and sides
+
+        let parts: Vec<&str> = dice.split('d').collect();
+        let num_rolls = parts[0].parse::<i32>().unwrap_or(1);
+        let num_sides = parts[1].parse::<i32>().unwrap_or(20);
+        
+        let modifier_operator = match cli.modifier_operator.as_deref() {
+            Some(modifier_operator) => modifier_operator,
+            None => "None",
+        };
+
+        let modifier_value = match cli.modifier_value.as_deref() {
+            Some(modifier) => modifier,
+            None => "None",
+        };
+
+        match cli.debug {
+            0 => {}
+            _ => {
+                println!("Value for dice: {dice}");
+                println!("Value for num_rolls {num_rolls}");
+                println!("Value for num_sides: {num_sides}");
+                println!("Modifier found: {modifier_operator}");
+                println!("Modifier value found: {modifier_value}");
+
+            }
+        }
+
+        let result = rand::thread_rng().gen_range(1..=20);
+
+        println!("{}", result);
+
+
     }
 
-    if let Some(modifier_operator) = cli.modifier_operator.as_deref() {
-        println!("Modifier found: {modifier_operator}");
-    }
-
-    if let Some(modifier_value) = cli.modifier_value.as_deref() {
-        println!("Modifier value found: {modifier_value}");
-    }  
-
-    let result = rand::thread_rng().gen_range(1..=20);
-
-    println!("{}", result);
+    println!("Thanks for rolling!");
 }
